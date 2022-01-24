a997e6b060ece5c04ff7415e3825a9a97562be56
badalpareta <badalpareta@outlook.com>
2022-01-24 23:58:37 +0530

    revised

--- script.js
+++ script.js
@@ -2,38 +2,22 @@ const buttonElement = document.querySelectorAll("button");
 const display = document.querySelector("#input");
 window.addEventListener('click', (e) => {
     let clickValue=e.target.value;
-    console.log(clickValue)
-    // if(clickValue==="equal")
-    // {
-    //     if(display.value!==""){
-    //         display.value = eval(display.value);
-    //      }
-    // }
-    // else if(clickValue==="clear")
-    // {
-    //     display.value = "";  
-        
-    // }
-    
-    // else if(clickValue=="1"||clickValue=="2"||clickValue=="+")
-    // {
-    //     display.value=display.value+clickValue;
-    // }
+
     switch(clickValue)
     {
-        case"equal":if(display.value!="")
-                        display.value=eval(display.value);
+        case"equal":if(display.placeholder!="")
+                        display.placeholder=eval(display.placeholder);
                         break;
-        case"clear":display.value="";
+        case"clear":display.placeholder="";
                     break;
         case"back":
-                        display.value=display.value.substring(0,display.value.length-1);
+                        display.placeholder=display.placeholder.substring(0,display.placeholder.length-1);
                         break;
-        case"pm":display.value=eval("-1"*eval(display.value));
+        case"pm":display.placeholder=eval("-1"*eval(display.placeholder));
                     break;
         default:if(clickValue==undefined)
         clickValue="";
-            display.value=display.value+clickValue;
+            display.placeholder=display.placeholder+clickValue;
     }
     
    })
